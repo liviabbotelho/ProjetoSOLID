b73fd1fedbb779ce8034b40a7e477bc4a2f6bcad
livia.botelho <livia.botelho@codenapp.com>
2023-06-13 17:02:31 -0300

    Implementando index.php para testar o código

--- app_carrinho_compras_b/index.php
+++ app_carrinho_compras_b/index.php
@@ -13,6 +13,51 @@ echo '<h3>Com SRP</h3>';
 
 $pedido = new Pedido();
 
+$item1 = new Item();
+$item2 = new Item();
+
+$item1->setDescricao('Porta copo');
+$item1->setValor(2.99);
+
+$item2->setDescricao('Lâmpada');
+$item2->setValor(8.50);
+
+$pedido->getCarrinhoCompra()->adicionarItem($item1);
+$pedido->getCarrinhoCompra()->adicionarItem($item2);
+
+echo '<h4>Pedido</h4>';
+
+echo '<pre>';
+print_r($pedido);
+echo '<pre>';
+
+echo '<h4>Itens do carrinho</h4>';
+echo '<pre>';
+print_r($pedido->getCarrinhoCompra()->getItens());
+echo '<pre>';
+
+echo '<h4>Valor do pedido</h4>';
+$total = 0;
+foreach($pedido->getCarrinhoCompra()->getItens() as $key => $item){
+   $total += $item->getValor();
+}
+echo $total;
+
+echo '<h4>Carrinho está válido?</h4>';
+echo $pedido->getCarrinhoCompra()->validarCarrinho();
+
+echo '<h4>Status do pedido:</h4>';
+echo $pedido->getStatus();
+
+echo '<h4>Confirmar pedido</h4>';
+echo $pedido->confirmar();
+
+echo '<h4>E-mail</h4>';
+if($pedido->getStatus() == 'confirmado'){
+   echo EmailService::dispararEmail();
+}
+
+
 /*
 print_r($carrinho1->exibirItens());
 echo 'Valor total= ' . $carrinho1->exibirValorTotal();
